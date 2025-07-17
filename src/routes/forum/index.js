2de94657a17321748f632aed5b59de98e3e7c509
longathelstan <121792617+longathelstan@users.noreply.github.com>
2025-07-17 20:28:05 +0700

    feat: Update Forum Index Routes

--- src/routes/forum/index.js
+++ src/routes/forum/index.js
@@ -4,11 +4,16 @@ const router = express.Router();
 const categoryRoutes = require('./category.routes');
 const topicRoutes = require('./topic.routes');
 const postRoutes = require('./post.routes');
-// const profileRoutes = require('./profile.routes'); // Will be added later
+const profileRoutes = require('./profile.routes');
+const { getForumStats } = require('../../controllers/forum/stats.controller');
 
+// Stats route (before other routes to avoid conflicts)
+router.get('/stats', getForumStats);
+
+// Mount sub-routes
 router.use('/categories', categoryRoutes);
 router.use('/topics', topicRoutes);
 router.use('/posts', postRoutes);
-// router.use('/profiles', profileRoutes);
+router.use('/profiles', profileRoutes);
 
 module.exports = router;
